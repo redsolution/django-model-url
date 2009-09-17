e6ae412ba89879e5a41cc96d2d1761ad92558bad
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-09-17 18:04:30 +0600

    Add class Replace to utils.py

--- modelurl/utils.py
+++ modelurl/utils.py
@@ -1,46 +1,69 @@
-from pages.models import Page, Content
 import re
 
-def get_changes_for_pages():
-    changes = []
-    for page in Page.objects.all():
-        url = page.get_absolute_url()
-        if not url:
-            continue
-        if len(url) > 1 and url[-1] == '/':
-            url = url[:-1]
-        url = re.escape(url)
-        repl = '{%% model_url \'pages.model.Page\' %d %%}' % page.id
-        changes.append((re.compile('"%s/?"' % url), '"%s"' % repl))
-        changes.append((re.compile('"%s/?#' % url), '"%s#' % repl))
-    return changes
+def generate_marco(model, id, function=None):
+    """
+    Return marco string for specified ``model`` and ``id``.
+    You can specified function to be called to get absolute url.
+    """
+    if function:
+        return '{@ %s.%s %s %s @}' % (model.__module__, model.__name__, id, function)
+    else:
+        return '{@ %s.%s %s @}' % (model.__module__, model.__name__, id)
+    
+class Replace(object):
+    """
+    Class to replace urls with macros.
+    """
 
+    def __init__(self, dict):
+        """
+        Set ``dict`` dictionary for replace operations.
+        Keys must be an url.
+        Value must be macro (use ``generate_marco`` function to get it).
+        """
+        self.source = dict
+        self.dict = None
+        self.list = None
+        self.regexp = None
+    
+    def get_dict(self):
+        if self.dict is None:
+            self.dict = {}
+            for key, value in self.source.iteritems():
+                self.dict[key.lower()] = value
+        return self.dict
 
-def apply_changes(body, changes):
-    for pattern, repl in changes:
-        body = re.sub(pattern, repl, body)
-    return body
+    def get_list(self):
+        """
+        Return list with urls and macros.
+        """
+        if self.list is None:
+            self.list = list(self.get_dict().iteritems())
+        return self.list
 
+    def get_regexp(self):
+        """
+        Return regexp for all urls.
+        """
+        if self.regexp is None:
+            self.regexp = re.compile('|'.join(['(%s)' % re.escape(key)
+                for key, value in self.get_list()]), re.IGNORECASE)
+        return self.regexp
 
-def process_pages(log=False, changes=None):
-    if changes is None:
-        changes = get_changes_for_pages()
-    if log:
-        old = open('modelurl.old', 'w')
-        new = open('modelurl.new', 'w')
-    for content in Content.objects.all():
-        if Content.objects.filter(
-            language=content.language, type=content.type, page=content.page).latest() != content:
-            continue
-        body = apply_changes(content.body, changes)
-        if log:
-            if body != content.body:
-                old.write('\n------%d----\n' % content.page.id)
-                old.write(content.body.encode('utf8'))
-                new.write('\n------%d----\n' % content.page.id)
-                new.write(body.encode('utf8'))
-        Content.objects.create_content_if_changed(
-            content.page, content.language, content.type, body)
-    if log: 
-        old.close()
-        new.close()
+    def url(self, value):
+        """
+        Return marco for specified ``value``.
+        Or raise KeyError exception if there is no marco for ``value``.
+        """
+        return self.get_dict()[value.lower()]
+    
+    def text(self, value):
+        """
+        Replace urls in ``text`` with marcos.
+        """
+        def replace(match):
+            for index, value in enumerate(match.groups()):
+                if value is not None:
+                    return self.get_list()[index]
+            return ''
+        return self.get_regexp().sub(replace, value)
