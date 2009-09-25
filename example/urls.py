91c6b5d484fdc6a40f1c92d4d9e925da36f5b4a1
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-09-25 19:03:59 +0600

    Fix example/urls.py

--- example/urls.py
+++ example/urls.py
@@ -6,10 +6,10 @@ for view in ['response', 'notfound', 'error', 'redirect_response',
     'redirect_notfound', 'redirect_redirect_response', 'redirect_cicle', 
     'permanent_redirect_response', 'http404', 'http500', 
     'request_true_response', 'request_false_response',]:
-    urlpatterns += patterns('example.views', url(r'^%s' % view, view, name=view))
+    urlpatterns += patterns('example.views', url(r'^%s$' % view, view, name=view))
 
 urlpatterns += patterns('example.views',
-    url(r'^page_by_id/(\d{1,8})', 'page_by_id', name='page_by_id'),
-    url(r'^item_by_id/(\d{1,8})', 'item_by_id', name='item_by_id'),
-    url(r'^item_by_barcode/(?P<barcode>\w+)', 'item_by_barcode', name='item_by_barcode'),
+    url(r'^page_by_id/(\d{1,8})$', 'page_by_id', name='page_by_id'),
+    url(r'^item_by_id/(\d{1,8})$', 'item_by_id', name='item_by_id'),
+    url(r'^item_by_barcode/(?P<barcode>\w+)$', 'item_by_barcode', name='item_by_barcode'),
 )
