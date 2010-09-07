bb9b4edc770d9c07ec0fe638ecb8288476e0e2a1
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-09-07 15:25:38 +0600

    Add requires to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 setup(
     name="redsolutioncms.django-model-url",
     version="0.1.0",
-    description=("Django model url" +
+    description=("Tool to provide recent urls for your movable objects from any stored html" +
         " with RedsolutionCMS integration"),
     license="LGPL",
     keywords="django model url",
@@ -38,7 +38,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking',
     ],
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['django-url-methods==0.1.0'],
     include_package_data=True,
     zip_safe=False,
     long_description=open('README').read(),
